feb9fe4fb21b4fa540f5635133b71277dd55088e
javite <refugiero@hotmail.com>
2025-12-24 14:14:01 -0300

    test mqtt

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -1,12 +1,19 @@
 import { Controller, Get } from '@nestjs/common';
 import { AppService } from './app.service';
+import { Ctx, MessagePattern, MqttContext, Payload } from '@nestjs/microservices';
 
 @Controller()
 export class AppController {
-  constructor(private readonly appService: AppService) {}
+  constructor(private readonly appService: AppService) { }
 
   @Get()
   getHello(): string {
     return this.appService.getHello();
+  }
+
+  @MessagePattern('test/hello')
+  testMqtt(@Payload() data: any, @Ctx() context: MqttContext) {
+    console.log(`[MQTT] Received message on topic "test/hello":`, data);
+    return `Hello from Backend! Received: ${data}`;
   }
 }
