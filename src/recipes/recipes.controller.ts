af789c8880de3507b4e26bede4595490b2d40af0
javite <refugiero@hotmail.com>
2026-01-01 22:15:00 -0300

    fix get program

--- src/recipes/recipes.controller.ts
+++ src/recipes/recipes.controller.ts
@@ -30,8 +30,8 @@ export class RecipesController {
       return { error: 'Device not found' };
     }
     const userId = req.user.userId;
-    const topic = `mars/${userId}/device/${device.serial_number}/getActualProgram`;
-    const responseTopic = `mars/devices/${device.serial_number}/data`;
+    const responseTopic = `mars/${userId}/device/${device.serial_number}/getActualProgram`;
+    const topic = `mars/devices/${device.serial_number}/data`;
     const body = {
       cmd: 'getActualProgram',
     };
